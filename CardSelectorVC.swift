2aa821da16b771a0fd80e8a465a34c4ee0385e73
unc-dan <firstdan8989@gmail.com>
2020-04-21 00:33:56 +0700

    Configure UI

---
+++ CardSelectorVC.swift
@@ -0,0 +1,30 @@
+//
+//  CardSelectorVC.swift
+//  Workout52
+//
+//  Created by Dan T on 21/04/2020.
+//  Copyright © 2020 Dan T. All rights reserved.
+//
+
+import UIKit
+
+class CardSelectorVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
